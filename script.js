c886491ab18d040912fb5e06a551f212f47825b5
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-18 08:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Function to handle the window resize event
+function handleResize() {
+  // Get the width and height of the window
+  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+  // Display the width and height
+  console.log('Width: ' + width);
+  console.log('Height: ' + height);
+}
+
+// Add event listener for window resize
+window.addEventListener('resize', handleResize);
+
+// Initial call to handleResize to get the width and height on page load
+handleResize();
