d0a1cc20cf6ccad7cc7faef6dacacc38b13e3ede
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-19 21:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 //your JS code here. If required.
-// Function to handle the window resize event
-function handleResize() {
-  // Get the width and height of the window
-  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+const sizeInfo = document.querySelector('#sizeInfo');
+const width = sizeInfo.querySelector('h1:first-child');
+const height = sizeInfo.querySelector('h1:last-child');
 
-  // Display the width and height
-  console.log('Width: ' + width);
-  console.log('Height: ' + height);
+function setScreenSize() {
+width.innerText = `Width: ${window.innerWidth}px`;
+height.innerText = `Height: ${window.innerHeight}px`;
 }
 
-// Add event listener for window resize
-window.addEventListener('resize', handleResize);
+window.addEventListener('resize', setScreenSize);
 
-// Initial call to handleResize to get the width and height on page load
-handleResize();
+setScreenSize();
